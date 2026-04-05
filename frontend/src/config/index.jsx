8774879aff7ab601d15a945a12cb702defb6ae49
Om_Asati <omasati2906@gmail.com>
2026-04-05 11:53:27 +0530

    Production: Re-updating BASE_URL to Render production URL

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:9091";
+export const BASE_URL = "https://carrierlink.onrender.com";
 
 export default axios.create({
    baseURL: BASE_URL,
