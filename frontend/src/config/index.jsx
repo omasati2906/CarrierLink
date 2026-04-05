9bf3f4a5047a13906781c19d9d5f7f54e61e7a7e
Om_Asati <omasati2906@gmail.com>
2026-04-05 12:08:10 +0530

    Production: Using NEXT_PUBLIC_BASE_URL for API configuration

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://carrierlink.onrender.com";
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:9090";
 
 export default axios.create({
    baseURL: BASE_URL,
